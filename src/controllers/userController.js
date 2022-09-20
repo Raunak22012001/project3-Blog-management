382201ea772b67922fef9573143eb7321cc6175c
unknown <akshataparghane@gmail.gmail.com>
2022-09-20 17:38:07 +0530

    login user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,81 +29,90 @@ const matchPass = function (value) {
     let regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/;
     return regex.test(value)
 }
+//----------------------------------------------Create user ----------------------------------------
 
 
-
-
-
-
-const createUser = async function(req,res){
+const createUser = async function(req,res)
+{
     try {
     
     let data = req.body;
     let {title, name, phone, email, password, address} = data;
 
-    
-     if(!isVAlidRequestBody(data) ) return res.status(400).send({status:false, message:"please enter user details"})
+    if(!isVAlidRequestBody(data) ) return res.status(400).send({status:false, message:"please enter user details"})
 
+    if(!isValid(title) ) return res.status(400).send({status:false, message:"Please enter title"})
+    if((title!=="Mr") && (!title=="Mrs") && (!title=="Miss")) return res.status(400).send({status:false, message:"title must be Mr, Mrs or Miss"})
+    
+    
+    if(!isValid(name) ) return res.status(400).send({status:false, message:"Please enter name"})
+    if(!checkName(name)) return res.status(400).send({status:false,message:"Please enter valid name"})
+    //Db calling
+    let duplicateName = await userModel.findOne({name:name})
+    if(duplicateName) return res.status(400).send({status:false, mssage:"name already exist"})
+    
 
-    if(!isValid(title) ) return res.status(400).send({status:false, message:"please use title"})
-    if((title!=="Mr") && (!title=="Mrs") && (!title=="Miss")) return res.status(400).send({status:false, message:"please use correct title"})
-    
-    
-    if(!isValid(name) ) return res.status(400).send({status:false, message:"please use name"})
-    if(!checkName(name)) return res.status(400).send({status:false,message:"don't use special char or number"})
-    
-
-    if(!isValid(phone) ) return res.status(400).send({status:false, message:"please use title"})
+    if(!isValid(phone) ) return res.status(400).send({status:false, message:"Please enter phone"})
     if(!phoneNub(phone)) return res.status(400).send({status:false,message:"Phone number is invalid"})
      //Db calling
     let checkUserByphone = await userModel.findOne({phone:phone})
-    if(checkUserByphone) return res.status(400).send({status:false, mssage:"thie phone number already exist"})
+    if(checkUserByphone) return res.status(400).send({status:false, mssage:"phone already exist"})
     
 
     
-    if(!isValid(email) ) return res.status(400).send({status:false, message:"please use title"})
-    if(!emailMatch(email)) return res.status(400).send({status:false,message:"please use valid email "})
+    if(!isValid(email) ) return res.status(400).send({status:false, message:"Please enter email"})
+    if(!emailMatch(email)) return res.status(400).send({status:false,message:"please enter valid email "})
     //Db calling
     let checkUserByemail = await userModel.findOne({email:email})
-    if(checkUserByemail) return res.status(400).send({status:false, message:"this email already exist"})
+    if(checkUserByemail) return res.status(400).send({status:false, message:"email already exist"})
     
     
-    if(!isValid(password) ) return res.status(400).send({status:false, message:"please use title"})
-    if(!matchPass(password)) return res.status(400).send({status:false,message:"please use valid password "})
+    if(!isValid(password) ) return res.status(400).send({status:false, message:"Please enter password"})
+    if(!matchPass(password)) return res.status(400).send({status:false,message:"Please enter valid password "})
         //Db calling
     let checkUserBypassword = await userModel.findOne({password:password})
-    if(checkUserBypassword) return res.status(400).send({status:false, message:"this user already exist"})
+    if(checkUserBypassword) return res.status(400).send({status:false, message:"password already exist"})
 
 
-    if(!isValid(address) ) return res.status(400).send({status:false, message:"please use title"})
+    if(!isValid(address) ) return res.status(400).send({status:false, message:"Please enter address"})
 
 
     let createuser = await userModel.create(data)
-    return res.status(201).send({status:true, data:createuser})
-} catch (error) {
+     return res.status(201).send({status:true,message:"User created successfull", data:createuser})
+} 
+catch (error) {
     return res.status(500).send({status:false, message:error.message})
         
 } 
 
 }
 
+//-------------------------------------User login -----------------------------------------------------
 
 const userlogin = async function(req, res)
 {
     try {
-        let userName = req.body.email;
-      let password = req.body.password;
-      if(!userName) return res.status(400).send({status: false, message: "Please enter userName"})
-      if(!password) return res.status(400).send({status: false, message: "Please enter password"})
-
-      let checkuser = await userModel.findOne({email: userName, password: password} )
-      if(!checkuser) return res.status(404).send({status: false, message: "Invalid userName or password"})
+    let userName = req.body.email;
+    let password = req.body.password;
 
 
-      const token = await jwt.sign({
-        userId : checkuser._id.toString(),
-        iat: Math.floor(Date.now()/1000), 
-        exp: Math.floor(Date.now()/1000)
+    if(!isValid(userName) ) return res.status(400).send({status:false, message:"please use title"})
+    if(!emailMatch(userName)) return res.status(400).send({status:false,message:"please use valid email "})
+    //Db calling
+   
+    if(!isValid(password) ) return res.status(400).send({status:false, message:"please use title"})
+    if(!matchPass(password)) return res.status(400).send({status:false,message:"please use valid password "})
+    //Db calling
+
+
+    let chekUser = await userModel.findOne({email:userName, password:password})
+    if(!(chekUser)) return res.status(400).send({status:false, message:"Invalid password"})
+
+
+    const token = await jwt.sign({
+        userId : chekUser._id.toString(),
+        iat: Math.floor(Date.now()/1000), //  create 
+        exp: Math.floor(Date.now()/1000)   // expire
       }, "Scretekeygroup22"
       )
       res.setHeader("x-auth-key", token);
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     title: {
         type: String,
-         unique: true, 
          enum:["Mr", "Mrs", "Miss"]
         },
   name: {
